ac57570f0a0ca45fb930c0f1d328f33032002ec0
Jesslyn Caroline <158556535+jesslyn-caroline@users.noreply.github.com>
2024-12-16 18:56:57 +0700

    Update Task 1-2.py

--- Task 1-2.py
+++ Task 1-2.py
@@ -1,4 +1,6 @@
 import os
+from prettytable import PrettyTable 
+
 
 def cls():
     if os.name == 'nt':
@@ -8,6 +10,28 @@ def cls():
 
 
 ### ==== Start of Handle Input ==== ###
+
+def checkNimValidity (nim):
+    if nim.count(" ") == len(nim):
+        raise ValueError("NIM tidak boleh kosong")
+    
+    if len(nim) != 9:
+        raise ValueError("NIM hanya boleh terdiri dari 9 karakter")
+    
+    num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    for i in range (len(nim)):
+        if nim[i] not in num:
+            raise ValueError("NIM hanya boleh berupa angka")
+            
+    year = int(nim[0:2])
+    if year > 24:
+        raise ValueError("Tahun pada NIM tidak valid")
+    
+    jurusan = ["111", "112", "113", "211", "212"]
+    if nim[2:5] not in jurusan:
+        raise ValueError("Kode jurusan di NIM tidak valid")
+    
+    return True
 
 def checkMajorValidity (major):
     if len(major) == major.count(' '):
@@ -59,19 +83,11 @@ def checkGenderValidity (jenisKelamin):
 ### ==== Start of Mhs Class ==== ###
 
 class Mhs:
-    def __init__ (self, nim, nama, jenisKelamin, nomorHp, kelas):
+    def __init__ (self, nim, nama, jenisKelamin, nomorHp):
         self.nim = nim
         self.nama = nama
         self.jenisKelamin = jenisKelamin
         self.nomorHp = nomorHp
-
-class NewMhs:
-  def __init__ (self, nama, jurusan, jenisKelamin, nomorHp, due):
-        self.nama = nama
-        self.jurusan = jurusan
-        self.jenisKelamin = jenisKelamin
-        self.nomorHp = nomorHp
-        self.due = due
 
 ### ==== End of Mhs Class ==== ###
 
@@ -88,32 +104,43 @@ class MhsLst:
         if len(self.lst) == 0:
             print("Tidak ada mahasiswa yang terdaftar")
         else:
+            table = PrettyTable()
+            table.field_names = ["NIM", "Nama", "Jenis Kelamin", "Nomor HP"]
             for i in self.lst:
-                print(f"NIM: {i.nim} - Nama: {i.nama} - Nomor HP: {i.nomorHp}")
+                table.add_row([i.nim, i.nama.title(), i.jenisKelamin, i.nomorHp])
+            print(table)
+    
+    def getLength (self):
+        return len(self.lst)
 
-
-class RolledMhs:
+class PaymentHistory:
     def __init__ (self):
-        self.lst = []
-      
-    def addToLst (self, mhs):
-        self.lst.append(mhs)
+        self.history = []
     
-    def printLst (self):
-        if len(self.lst) == 0:
-            print("Tidak ada mahasiswa yang terdaftar")
+    def addToHistory (self, mhs):
+        self.history.append(mhs)
+
+    def checkHistory (self):
+        if len(self.history) == 0:
+            print("Tidak ada mahasiswa yang telah membayar")
         else:
-            for i in self.lst:
-                print(f"Nama: {i.nama} - Nomor HP: {i.nomorHp} - Total: {i.due}")
+            table = PrettyTable()
+            table.field_names = ["NIM", "Nama", "Nominal"]
+            for i in self.history:
+                table.add_row([i[0].nim, i[0].nama, i[1]])
+
+            print(table)
     
-    def search (self, nama):
-        for i in self.lst:
-            if i.nama == nama:
+    def search (self, nim):
+        for i in self.history:
+            if i[0].nim == nim:
                 return i
-        
+            
         return False
+
               
 ### ==== End of Mhs List Class ==== ###
+
 
 ### ==== Start of Composite Class ==== ###
 
@@ -127,7 +154,7 @@ class Composite:
         self.child.append(leaf)
     
     def detail(self, level = 0):
-        print('\t'* level, end="")
+        print('  '* level, end="")
         print(f'- {self.name}')
         for i in self.child:
             self.total += i.detail(level + 1)
@@ -141,7 +168,7 @@ class Leaf:
         self.price = price
     
     def detail(self, level):
-        print('\t'* level, end="")
+        print(' '* level, end="")
         print(f'- {self.name}: {self.price}')
         return self.price
 
@@ -169,21 +196,48 @@ def getBill():
     print(f"Total biaya: {total}")
     return total
     
+def getNim (year, jurusan):
+    year = str(year % 2000)
+    kodeJurusan = None
+
+    if jurusan == 'teknik informatika':
+        kodeJurusan = '111'
+    elif jurusan == 'sistem informasi':
+        kodeJurusan = '112'
+    elif jurusan == 'teknologi informasi':
+        kodeJurusan = '113'
+    elif jurusan == 'akuntansi':
+        kodeJurusan = '211'
+    else:
+        kodeJurusan = '212'
+    
+    id = mhsLst.getLength() + 1
+    if id < 10:
+        id = f'000{id}'
+    elif id < 100:
+        id = f'00{id}'
+    elif id < 1000:
+        id = f'0{id}'
+    else:
+        id = f'{id}'
+
+    return f'{year}{kodeJurusan}{id}'
+
 
 ### ==== Main Program ==== ###
 
 mhsLst = MhsLst()
-rolledMhs = RolledMhs()
-
+paymentHistory = PaymentHistory()
+total = getBill()
 if __name__ == "__main__":
     while True:
         cls()
         print(f"Menu")
         print(f"="*80)
         print(f"1. Tambah Mahasiswa Baru")
-        print(f"2. Daftar Mahasiswa yang mendaftar")
+        print(f"2. History pembayaran Mahasiswa")
         print(f"3. Daftar Mahasiswa Baru")
-        print(f"4. Update biaya pendaftaran Mahasiswa")
+        print(f"4. Bayar cicilan uang kuliah")
         print(f"5. Exit")
         print(f"="*80)
         
@@ -255,40 +309,69 @@ if __name__ == "__main__":
             print()
             print()
             
-            print(f"Berikut rincian biaya kuliah")
             total = getBill()
-            newMhs = NewMhs(nama, jurusan, jenisKelamin, nomorHp, total)
-            rolledMhs.addToLst(newMhs)
+
+            valid = False
+            while not valid:
+                try:
+                    full = input("Apakah anda ingin membayar penuh? (Y/N): ").upper()
+                    if len(full) == full.count(' '):
+                        raise ValueError("Tidak boleh kosong")
+                    if full not in ['Y', 'N']:
+                        raise ValueError("Hanya ada Y / N")
+                    
+                    valid = True
+
+                except ValueError as err:
+                    print(f'Invalid input: {err}')
+            
+            if full == 'Y':
+                total = total - (total * 0.1)
+                print("Biaya pendaftaran telah dibayar penuh dengan potongan diskon 10%")
+            else:
+                print("Mahasiswa tersebut telah didaftarkan")
+
+            nim = getNim(2023, jurusan)
+            mhs = Mhs(nim, nama, jenisKelamin, nomorHp)
+
+            mhsLst.addToLst(mhs)
+            if full == 'Y':
+                paymentHistory.addToHistory([mhs, int(total)])
+            else:
+                paymentHistory.addToHistory([mhs, int(total / 4)])
+            
+            print(f"Mahasiswa tersebut telah terdaftar dengan NIM: {nim}")
         
         elif op == 2:
-            print("Daftar Mahasiswa yang Mendaftar")
-            rolledMhs.printLst()
+            print("History pembayaran Mahasiswa")
+            paymentHistory.checkHistory()
 
         elif op == 3:
             print("Daftar Mahasiswa Baru")
             mhsLst.printLst()
 
         elif op == 4:
-            nameValid = False
-            while not nameValid:
+            nimValid = False
+            while not nimValid:
                 try:
-                    nama = input('Nama: ').lower()
-                    nameValid = checkNameValidity(nama)
+                    nim = input("NIM: ")
+                    nimValid = checkNimValidity(nim)
                 
                 except ValueError as err:
-                    print(f'Invalid input: {err}')
+                    print(f"Invalid input: {err}")
             
             print()
             
-            found = rolledMhs.search(nama)
+            found = paymentHistory.search(nim)
             if found:
-                found.due = 0
-                print(f"Biaya pendaftaran untuk {nama} telah diupdate.")
+                paymentHistory.addToHistory([found, total / 4])
+                print("Terima kasih telah membayar")
             else:
-                print(f'Mahasiswa tersebut tidak terdaftar.')
+                print("Mahasiswa belum terdaftar")
         
         elif op == 5:
             print(f"Thank you!")
             break
         
+        print()
         any = input("[ Press any button to continue ]")
